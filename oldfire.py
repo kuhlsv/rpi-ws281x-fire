a15c7143e6090d2c85620745cd03a8fdeda3b1e9
sven <kuhlmann.sven94@web.de>
2024-08-20 12:24:28 +0200

    renamed files

--- oldfire.py
+++ oldfire.py
@@ -10,7 +10,7 @@ LED_COUNT = 144        # Number of LED pixels.
 LED_PIN = 18          # GPIO pin connected to the pixels (18 uses PWM!).
 LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA = 10          # DMA channel to use for generating signal (try 10)
-LED_BRIGHTNESS = 200  # Set to 0 for darkest and 255 for brightest
+LED_BRIGHTNESS = 180  # Set to 0 for darkest and 255 for brightest
 LED_INVERT = False    # True to invert the signal (when using NPN transistor level shift)
 LED_CHANNEL = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 SENSOR_PIN = 23       # Sensor pin for KY-002
@@ -63,7 +63,7 @@ class Fireplace:
         return (color >> 16) & 0xff, (color >> 8) & 0xff, color & 0xff
 
 class Fireflame:
-    normal_fire_color = Color(100, 30, 0)
+    normal_fire_color = Color(80, 35, 0)
     big_fire_flame = Color(220, 80, 0)
     off_color = Color(0, 0, 0)
     strip = None
@@ -77,8 +77,8 @@ class Fireflame:
             color = self.normal_fire_color
         for i in range(LED_COUNT):
             self.add_color(i, color)
-            r = random.randint(0, 30) + 50  # (0,80)
-            diff_color = Color(r, r // 3, r // 4) #(r,r 2, r 2)
+            r = random.randint(0, 80)
+            diff_color = Color(r, r // 2, r // 2)
             self.subtract_color(i, diff_color)
         self.strip.show()
 
@@ -119,87 +119,54 @@ class Fireflame:
         time.sleep(duration)        # Hold the flame for the specified duration
         self.clear()                # Clear the flame
         self.strip.show()
-        #self.draw()
 
-def debounce(sensor_pin, debounce_time_ms=50):
-    """
-    Debounces the input from a GPIO sensor pin.
+def alarm(timer):
+    print()
+    print('Bewegung')
+    print()
+    print('Ruhe')
 
-    Args:
-        sensor_pin (int): The GPIO pin number where the sensor is connected.
-        debounce_time_ms (int): The debounce time in milliseconds. Default is 50 ms.
-
-    Returns:
-        bool: The debounced state of the sensor (True if triggered, False otherwise).
-    """
-    stable_state = GPIO.input(sensor_pin)
-    last_state = stable_state
-    start_time = time.time()
-
-    while True:
-        current_state = GPIO.input(sensor_pin)
-
-        if current_state != last_state:
-            # State has changed, reset the debounce timer
-            start_time = time.time()
-            last_state = current_state
-
-        # Check if the state has been stable for the debounce time
-        elapsed_time = (time.time() - start_time) * 1000.0
-        if elapsed_time >= debounce_time_ms:
-            if current_state == stable_state:
-                return stable_state
-            else:
-                stable_state = current_state
-                return stable_state
-
-def flame_up(fire):
-    # Occasionally trigger a big flame effect
-    if random.random() < 0.95:  # 95% chance to trigger big flame
-        fire.big_flame(duration=0.9)  # Adjust duration as needed
-        time.sleep(random.uniform(0.05, 0.20))
-
-def shake_wrapper(fire):
-    # Wrappes the callback function for custom a parameter
-    def wrapped_callback(channel):
-        shake(channel, fire)
-    return wrapped_callback
-
-def shake(channel):
+def shake(pin):
     # Entprellfunktion: Timer setzen (200 ms)
     # Verhindert wiederholte ausführung
-    debounced_state = debounce(SENSOR_PIN)
-    if debounced_state:
-        print("Vibration detected!")
-        flame_up(fire)
-    else:
-        print("Debounced!")
-
-# Init LEDs with NeoPixel
-strip = PixelStrip(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
-strip.begin()
-fire = Fireflame(strip)
-fire.draw()
-time.sleep(random.uniform(0.05, 0.20))
+    timer.init(mode=Timer.ONE_SHOT, period=200, callback=alarm)
 
 # Main
 if __name__ == '__main__':
-    print("Start fire show")
     # Process arguments
     parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--clear', action='store_true', help='clear the display on exit')
     args = parser.parse_args()
+
+    # Init LEDs with NeoPixel
+    strip = PixelStrip(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+    strip.begin()
+    # Create an instance of the NeoFire class
+    fire = Fireflame(strip)
 
     # Init vibration Sensor with KY-002
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(SENSOR_PIN, GPIO.IN, pull_up_down = GPIO.PUD_UP)
     GPIO.add_event_detect(SENSOR_PIN, GPIO.FALLING, callback=shake, bouncetime=100) 
 
+    print('Press Ctrl-C to quit.')
+    if not args.clear:
+        print('Use "-c" argument to clear LEDs on exit')
+
     try:
         while True:
-            time.sleep(1)
-            fire.draw()
+           #fire.draw()
+           #time.sleep(random.uniform(0.05, 0.2))
+
+           # Simulate normal fire flickering
+           fire.draw()
+           time.sleep(random.uniform(0.05, 0.20))
+
+           # Occasionally trigger a big flame effect
+           if random.random() < 0.1:  # 10% chance to trigger big flame
+               fire.big_flame(duration=0.2)  # Adjust duration as needed
+
     except KeyboardInterrupt:
-         print("Program terminated!")
-    finally:
-         fire.clear()
-         GPIO.cleanup()
+        if args.clear:
+            colorWipe(strip, Color(0, 0, 0), 10)
+            GPIO.cleanup()
